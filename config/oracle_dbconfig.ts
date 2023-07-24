bd75de0d15d4df7d493a8d03fa0a9a249c94b546
gaineee <gain.ex3@gmail.com>
2023-07-24 16:39:09 +0900

    init

--- config/oracle_dbconfig.ts
+++ config/oracle_dbconfig.ts
@@ -1,11 +1,4 @@
 module.exports = {
   // 혹시몰라서 보안때메 지움
 
-  "IRDEV": {
-    user : "PIS_DEV",
-    password: "PIS_DEVDB#1",
-    connectString: "172.18.240.21:3521/IRDEV",
-    externalAuth: false
-  },
-
 };
